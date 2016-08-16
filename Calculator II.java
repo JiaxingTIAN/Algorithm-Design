c0b25b28456e4beb4252163976083fd777e18b50
Jiaxing <tian.jiaixng@gmail.com>
2016-08-16 03:55:52 -0400

    Create Calculator II.java

---
+++ Calculator II.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public int calculate(String s) {
+        //Using a stack to calculate the * and / first and store the result in the stack
+        //Sum up all the number in the stack
+        if(s.length()==0||s==null) return 0;
+        Stack<Integer> stack = new Stack();
+        char[] str = s.toCharArray();
+        int len = s.length();
+        int num = 0;
+        char sign = '+';
+        for(int i=0;i<len;i++){
+            if(Charater.isDigit(str[i]))
+                num = num*10 + str[i];
+            if((!Charater.isDigit(str[i]) && str[i]!=' ')||i==len-1){
+                if(sign=='+') stack.push(num);
+                else if(stack=='-') stack.push(-num);
+                else if(stack=='*') stack.push(stack.pop()*num);    
+                else if(stack=='/') stack.push(stack.pop()/num);
+                sign = str[i];
+                num=0;
+            }
+        }
+        int res = 0;
+        for(int i:stack) res+=i;
+        return res;
+    }
+}
