afb1f058ad0a432d85361937279055c4422003e4
Jiaxing <tian.jiaixng@gmail.com>
2016-08-16 16:32:15 -0400

    Update Reverse List.java

--- Reverse List.java
+++ Reverse List.java
@@ -1,1 +1,13 @@
 public class Solution{
+  public ListNode reverse(ListNode head){
+    if(head==null||head.next==null) return head;
+    ListNode pre = null, cur = head;
+    while(cur!=null){
+      ListNode tmp = cur.next;
+      cur.next = pre;
+      pre = cur;
+      cur = tmp;
+    }
+    return pre;
+  }
+}
