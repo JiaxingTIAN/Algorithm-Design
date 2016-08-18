28618a05eeeb424a08c36c54b385034281ea0f02
Jiaxing <tian.jiaixng@gmail.com>
2016-08-18 17:43:20 -0400

    Update Permutation.java

--- List/Permutation.java
+++ List/Permutation.java
@@ -1,1 +1,22 @@
-
+public class Solution{
+  //Insert one number from the nums to the result each time
+  public List<List<Integer>> permute(int[]nums){
+    List<List<Integer> ans = new ArrayList();
+    if(nums.length==0) return ans;
+    List<Integer> list = new ArrayList();
+    list.add(nums[0]);
+    ans.add(list);
+    for(int i=1;i<nums.length;i++){
+      List<List<Integer>> new_ans = new ArrayList();
+      for(int j=0;j<=i;j++){  //pick the location for insert
+        for(List<Integer> n:ans){
+          List<Integer> newList = new ArrayList();
+          newList.add(j, nums[i]);
+          new_ans.add(newList);
+        }
+      }
+      ans = new_ans;
+    }
+    return ans;
+  }
+}
