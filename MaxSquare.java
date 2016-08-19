845067f8db5ffe82b4a078186c7038746ae453da
Jiaxing <tian.jiaixng@gmail.com>
2016-08-19 02:35:56 -0400

    Update MaxSquare.java

--- MaxSquare.java
+++ MaxSquare.java
@@ -1,1 +1,17 @@
-
+public class Solution{
+  public int maxSquare(int[][]matrix){
+    if(matrix.length==0) return 0;
+    int n = matrix.length, m = matrix[0].length;
+    int[][] dp = new int[n+1][m+1];
+    int max = 0;
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+        if(matrix[i][j]=='1'){
+          dp[i+1][j+1] = Math.min(dp[i][j], Math.min(dp[i+1][j],dp[i][j+1]));
+          max = Math.max(max, dp[i+1][j+1]);
+        }
+      }
+    }
+    return max*max;
+  }
+}
