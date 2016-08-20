d265e961823e15b329f4bdc0bf1ff8e98908d385
Jiaxing <tian.jiaixng@gmail.com>
2016-08-20 02:06:10 -0400

    Update RestoreIP.java

--- RestoreIP.java
+++ RestoreIP.java
@@ -1,1 +1,21 @@
-
+public class Solution{
+  public List<String> restore(String s){
+    int len = s.length();
+    List<String> res = new LinkedList();
+    for(int i=1;i<4 && i<len-2;i++){
+      for(int j=i+1;j<i+4 && j<len-1;j++){
+        for(int k=j+1;k<j+4 && k<len;K++){
+          String s1=s.substring(0,i), s2=s.substring(i,j), s3 = s.substring(j,k),s4=s.substring(k,len);
+          if(valid(s1)&&valid(s2)&&valid(s3)&&valid(s4))
+            res.add(s1+"."+s2+"."+s3+"."+s4);
+        }
+      }
+    }
+    return res;
+  }
+  public boolean valid(String s){
+    if(s.length()>3||s.length==0||(s.charAt(0)=='0'&&s.length>1)||Integer.parseInt(s)>255)
+      return false;
+    return true;
+  }
+}
