3aa828bac63ddb6d25aaf4add30c468ff1340a2e
Jiaxing <tian.jiaixng@gmail.com>
2016-08-22 03:48:12 -0400

    Create ReconstructItineray.java

---
+++ ReconstructItineray.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<String> findItinerary(String[][] tickets) {
+        Map<String, PriorityQueue<String>> v = new HashMap();
+        for(String[] str:tickets){
+            if(!v.containsKey(str[0]))
+                v.put(str[0], new PriorityQueue<String>((s1,s2)->(s1.compareTo(s2))));
+            v.get(str[0]).add(str[1]);
+        }
+        List<String> res = new LinkedList();
+        String start = "JFK";
+        dfs(tickets, res, v, start);
+        return res;
+    }
+    public void dfs(String[][]tickets, List res, Map<String, PriorityQueue<String>> v, String start){
+        while(v.containsKey(start) && !v.get(start).isEmpty()){
+            dfs(tickets, res, v, v.get(start).poll());
+        }
+        res.add(0, start);
+    }
+}
