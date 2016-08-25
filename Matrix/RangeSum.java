18b3ad2cf1ce439b444d06b66fe2c5b65f32f901
Jiaxing <tian.jiaixng@gmail.com>
2016-08-24 23:14:12 -0400

    Update RangeSum.java

--- Matrix/RangeSum.java
+++ Matrix/RangeSum.java
@@ -1,8 +1,8 @@
 public class NumMatrix {
 
-    private[][] sum;
+    private int [][] sum;
     public NumMatrix(int[][] matrix) {
-        sum = new int[matrix.length+1][matrix[0].length+1];
+        if(matrix.length!=0)sum = new int[matrix.length+1][matrix[0].length+1];
         for(int i=0;i<matrix.length;i++){
             int s = 0;
             for(int j=0;j<matrix[0].length;j++){
@@ -14,6 +14,6 @@ public class NumMatrix {
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum[row2][col2] - sum[row1][col2] - sum[row2][col1] + sum[row1][col1];
+        return sum[row2+1][col2+1] - sum[row1][col2+1] - sum[row2+1][col1] + sum[row1][col1];
     }
 }
