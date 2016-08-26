2dbb7252d515e8c07ef618ddb556e896a91279e4
Jiaxing <tian.jiaixng@gmail.com>
2016-08-25 23:56:45 -0400

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,5 +1,6 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //Using Hash Table
         int[] result = new int[2];
         HashMap<Integer,Integer> map = new HashMap<Integer, Integer>();
         
