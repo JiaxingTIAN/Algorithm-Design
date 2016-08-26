395ed1411e3a5132395353570a75ea42bf0692a7
Jiaxing <tian.jiaixng@gmail.com>
2016-08-26 02:57:24 -0400

    Update TriangleCount.java

--- Arrays/TriangleCount.java
+++ Arrays/TriangleCount.java
@@ -1,1 +1,24 @@
+public class Solution {
+    /**
+     * @param S: A list of integers
+     * @return: An integer
+     */
+    public int triangleCount(int S[]) {
+        // write your code here
+        Arrays.sort(S);
+        int count = 0;
+        for(int i=2;i<S.length;i++){
+            int left = 0, right = i-1;
+            while(left<right){
+                if(S[left]+S[right]>S[i]){
+                    count += right - left;
+                    right--;
+                }
+                else
+                    left++;
+            }
+        }
+        return count;
+    }
+}
 
