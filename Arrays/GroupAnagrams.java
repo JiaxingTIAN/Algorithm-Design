d637305fd32fdff1de38299e6b45c4a95577d40a
Jiaxing <tian.jiaixng@gmail.com>
2016-08-27 02:40:43 -0400

    Update GroupAnagrams.java

--- Arrays/GroupAnagrams.java
+++ Arrays/GroupAnagrams.java
@@ -1,1 +1,16 @@
-public class Solution
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs==null||strs.length==0)
+            return new LinkedList<List<String>>();
+        //use hashmap to detect whether the sorted word has anagrams and group together
+        Map<String, List<String>> map = new HashMap<>();
+        for(String str:strs){
+            char[] ch = str.toCharArray();
+            Arrays.sort(ch);
+            String key = String.valueOf(ch);
+            map.putIfAbsent(key, new LinkedList<String>());
+            map.get(key).add(str);
+        }
+        return new LinkedList<List<String>>(map.values());
+    }
+}
