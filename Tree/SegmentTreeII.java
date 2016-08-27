567d2afa7d4b80531bfa8a764f56d20af4f0c709
Jiaxing <tian.jiaixng@gmail.com>
2016-08-27 12:17:56 -0400

    Create SegmentTreeII.java

---
+++ Tree/SegmentTreeII.java
@@ -0,0 +1,28 @@
+public class Solution {
+    /**
+     *@param A: a list of integer
+     *@return: The root of Segment Tree
+     */
+    public SegmentTreeNode build(int[] A) {
+        // write your code here
+        return buildTree(0, A.length - 1, A);
+    }
+
+    public SegmentTreeNode buildTree(int start, int end, int[] A) {
+        if (start > end)
+            return null;
+
+        if (start == end) {
+            return new SegmentTreeNode(start, end, A[start]);
+        }
+        SegmentTreeNode node = new SegmentTreeNode(start, end, A[start]);
+        int mid = (start + end) / 2;
+        node.left = this.buildTree(start, mid, A);
+        node.right = this.buildTree(mid + 1, end, A);
+        if (node.left != null && node.left.max > node.max)
+            node.max = node.left.max;
+        if (node.right != null && node.right.max > node.max)
+            node.max = node.right.max;
+        return node;
+    }
+}
