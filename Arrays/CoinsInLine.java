488a0f0caaf7bc047d820d1c98a6aeaa1ac0d852
Jiaxing <tian.jiaixng@gmail.com>
2016-09-15 16:12:57 -0400

    Update CoinsInLine.java

--- Arrays/CoinsInLine.java
+++ Arrays/CoinsInLine.java
@@ -29,3 +29,36 @@ public class Solution {
         return dp[n];    
     }
 }
+public class Solution2{
+    /**
+     * @param n: an integer
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int n) {
+        // write your code here
+        
+        int[] dp = new int[n+1];
+        
+        return search(n, dp) == 2;
+    }
+    
+    
+    //0 for empty; 1 for false; 2 for true;
+    public int search(int n, int[] dp){
+        if(dp[n]!=0)
+            return dp[n];
+            
+        if(n==0)
+            dp[n] = 1;
+        else if(n==1 || n==2)
+            dp[n] = 2;
+        else 
+            //you are intelligent you choose the best move
+            if(search(n-1, dp)==1 || search(n-2, dp)==1)
+                dp[n] = 2;
+            else 
+                dp[n] = 1;
+                
+        return dp[n];    
+    }
+}
