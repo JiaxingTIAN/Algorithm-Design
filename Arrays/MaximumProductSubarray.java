568943c8bdcba05bbc73c0315ee4bfda953059dc
Jiaxing <tian.jiaixng@gmail.com>
2016-09-16 19:30:34 -0400

    Use DP optimization

--- Arrays/MaximumProductSubarray.java
+++ Arrays/MaximumProductSubarray.java
@@ -3,6 +3,7 @@ public class Solution {
      * @param nums: an array of integers
      * @return: an integer
      */
+    //Brute Force Solution
     public int maxProduct(int[] nums) {
         // write your code here
         if(nums == null||nums.length == 0)
@@ -18,5 +19,33 @@ public class Solution {
             }
         }
         return product;
+    }
+    
+    
+}
+//Optimized for linear Compliexity with DP
+public class Solution {
+    /**
+     * @param nums: an array of integers
+     * @return: an integer
+     */
+    public int maxProduct(int[] nums) {
+        // write your code here
+        //Keep Record of both min and max
+        //Since negative and negative will yield pos
+        if(nums == null||nums.length == 0)
+            return 0;
+        int n = nums.length;
+        int ans = Integer.MIN_VALUE;
+        int max = 1, preMax = 1;
+        int min = 1, preMin = 1;
+        for(int i=0; i<n; i++){
+            max = Math.max(Math.max(preMax*nums[i], preMin*nums[i]), nums[i]);
+            min = Math.min(Math.min(preMax*nums[i], preMin*nums[i]), nums[i]);
+            preMax = max;
+            preMin = min;
+            ans = Math.max(ans, max);
+        }
+        return ans;
     }
 }
