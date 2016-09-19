ce99b1aeca4c4471a9a3441b91012857dd578b8f
Jiaxing <tian.jiaixng@gmail.com>
2016-09-19 01:32:45 -0400

    Update ReconstructLine.java

--- Arrays/ReconstructLine.java
+++ Arrays/ReconstructLine.java
@@ -1,61 +1,67 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
+
+	import java.util.Arrays;
+	import java.util.Comparator;
+	import java.util.LinkedList;
+	import java.util.List;
 
 
-public class OrderTallPeople {
+public class ReconstructLine {
 
-	private static class Person {
-		String name;
-		int height;
-		int tallCount;
-		
-		Person(int h, int t, String n){
-			name = n;
-			height = h;
-			tallCount = t;
+		private static class Person {
+			String name;
+			int height;
+			int tallCount;
+			
+			Person(int h, int t, String n){
+				name = n;
+				height = h;
+				tallCount = t;
+			}
+			
+			@Override
+			public String toString() {
+				return "<"+name+","+height+"," +tallCount+">";
+			}
 		}
-		
-		@Override
-		public String toString() {
-			return "<"+name+","+height+"," +tallCount+">";
-		}
-	}
-
-	static class HeightComparator implements Comparator<Person> {
-		public int compare(Person p1, Person p2){
-			int ret  = p2.height - p1.height;
-			return (ret == 0)?p1.tallCount - p2.tallCount:ret;
-		}
-	}
-
-
-	public static List<Person> orderByHeightAndTaller(Person[] people){
-		Arrays.sort(people, new HeightComparator());
-		LinkedList<Person> ret = new LinkedList<>();
-		for(Person p: people){
-			ret.add(p.tallCount,p);
-		}
-		return ret;
-	}
-
-	public static void main(String[] args) {
-		Person [] people = {
-				new Person(6,2,"A"),
-				new Person(1,4,"B"),
-				new Person(11,0,"C"),
-				new Person(5,1,"D"),
-				new Person(5,3,"K"),
-				new Person(10,0,"E"),
-				new Person(4,0,"F")
-				
-				
+		//Sort the number of tall people in ascending order
+		//Sort the height according to descending order
+		public static Comparator<Person> cmp = new Comparator<Person>(){
+			public int compare(Person p1, Person p2){
+				if(p1.height != p2.height){
+					return p2.height - p1.height;
+				}
+				return p1.tallCount - p2.tallCount;
+			}
 		};
-		System.out.println(orderByHeightAndTaller(people));
+
+
+		public static List<Person> orderByHeightAndTaller(Person[] people){
+			Arrays.sort(people, cmp);
+			List<Person> list = new LinkedList<>();
+			for(Person p:people){
+				list.add(p.tallCount, p);
+			}
+			return list;
+		}
+
+		public static void main(String[] args) {
+			Person [] people = {
+					new Person(6,2,"A"),
+					new Person(1,4,"B"),
+					new Person(11,0,"C"),
+					new Person(5,1,"D"),
+					new Person(5,3,"K"),
+					new Person(10,0,"E"),
+					new Person(4,0,"F")
+					
+					
+			};
+			System.out.println(orderByHeightAndTaller(people));
+			
+			
+		}
 		
 		
-	}
-	
-	
 }
+
+
