16124c6db85b8b1f6a0bd3de1e214bfdfe1df777
Jiaxing <tian.jiaixng@gmail.com>
2016-09-22 00:35:40 -0400

    Update SlidingWindowMax.java

--- Arrays/SlidingWindowMax.java
+++ Arrays/SlidingWindowMax.java
@@ -25,3 +25,42 @@ public class Solution {
         return ans;
     }
 }
+
+//Optimization with deque -> O(n) time complexity
+public class Solution {
+    /**
+     * @param nums: A list of integers.
+     * @return: The maximum number inside the window at each moving.
+     */
+    public void inQueue(Deque<Integer> deque, int num){
+        while(!deque.isEmpty() && num > deque.peekLast()){
+            deque.pollLast();
+        }
+        deque.offerLast(num);
+    }
+    public void outQueue(Deque<Integer> deque, int num){
+        if(num == deque.peekFirst())
+            deque.pollFirst();
+    }
+     
+    public ArrayList<Integer> maxSlidingWindow(int[] nums, int k) {
+        // write your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        Deque<Integer> deque = new ArrayDeque<>();
+        
+        if(nums == null || nums.length == 0){
+            return ans;
+        }
+        
+        for(int i=0; i<nums.length; i++){
+            inQueue(deque, nums[i]);
+            
+            if(i >= k-1){
+                ans.add(deque.peekFirst());
+                outQueue(deque, nums[i-k+1]);
+            }
+        }
+        
+        return ans;
+    }
+}
