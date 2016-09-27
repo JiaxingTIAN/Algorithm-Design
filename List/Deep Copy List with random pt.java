3ea2a2d1c5c335b5a0af9bced170643cb7f2cc90
Jiaxing <tian.jiaixng@gmail.com>
2016-09-27 16:27:41 -0400

    Post a easier version

--- List/Deep Copy List with random pt.java
+++ List/Deep Copy List with random pt.java
@@ -42,3 +42,44 @@ public class Solution {
         return dummy.next;
     }
 }
+
+
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null){
+            return null;
+        }
+        //Use hashmap as a cache
+        Map<RandomListNode, RandomListNode> map = new HashMap<>();
+        //Copy all the node and put the in the map
+        RandomListNode cur = head;
+        while(cur != null){
+            RandomListNode copy = new RandomListNode(cur.label);
+            map.put(cur, copy);
+            cur = cur.next;
+        }
+        //Add connections next and random
+        cur = head;
+        while(cur != null){
+            RandomListNode node = map.get(cur);
+            node.next = map.get(cur.next);
+            
+            if(cur.random != null){
+                node.random = map.get(cur.random);
+            }else{
+                node.random = null;
+            }
+            cur = cur.next;
+        }
+        
+        return map.get(head);
+    }
+}
