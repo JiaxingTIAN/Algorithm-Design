cf92f4dedba004cea3ddc29849c3abc8d8c791a4
Jiaxing <tian.jiaixng@gmail.com>
2016-10-01 13:54:37 -0400

    Update Walls&Gates.java

--- Matrix/Walls&Gates.java
+++ Matrix/Walls&Gates.java
@@ -86,3 +86,18 @@ public class Solution {
         }
     }
 }
+//DFS version
+public void wallsAndGates(int[][] rooms) {
+    for (int i = 0; i < rooms.length; i++)
+        for (int j = 0; j < rooms[0].length; j++)
+            if (rooms[i][j] == 0) helper(rooms, i, j, 0);
+}
+
+private void helper(int[][] rooms, int i, int j, int d) {
+    if (i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] < d) return;
+    rooms[i][j] = d;
+    helper(rooms, i - 1, j, d + 1);
+    helper(rooms, i + 1, j, d + 1);
+    helper(rooms, i, j - 1, d + 1);
+    helper(rooms, i, j + 1, d + 1);
+}
