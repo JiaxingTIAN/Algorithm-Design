5cc2b2c174040c8f565e6181c347792af7dda79a
Jiaxing <tian.jiaixng@gmail.com>
2016-10-01 14:13:40 -0400

    Update Walls&Gates.java

--- Matrix/Walls&Gates.java
+++ Matrix/Walls&Gates.java
@@ -87,41 +87,24 @@ public class Solution {
     }
 }
 //DFS version
-public void wallsAndGates(int[][] rooms) {
-    for (int i = 0; i < rooms.length; i++)
-        for (int j = 0; j < rooms[0].length; j++)
-            if (rooms[i][j] == 0) helper(rooms, i, j, 0);
-}
-
-private void helper(int[][] rooms, int i, int j, int d) {
-    if (i < 0 || i >= rooms.length || j < 0 || j >= rooms[0].length || rooms[i][j] < d) return;
-    rooms[i][j] = d;
-    helper(rooms, i - 1, j, d + 1);
-    helper(rooms, i + 1, j, d + 1);
-    helper(rooms, i, j - 1, d + 1);
-    helper(rooms, i, j + 1, d + 1);
-}
-
-public static final int[] d = {0, 1, 0, -1, 0};
+int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 public void wallsAndGates(int[][] rooms) {
-    if (rooms.length == 0) return;
-    int m = rooms.length, n = rooms[0].length;
-
-    Deque<Integer> queue = new ArrayDeque<>();
-    for (int i = 0; i < m; ++i)
-        for (int j = 0; j < n; ++j)
-            if (rooms[i][j] == 0) queue.offer(i * n + j); // Put gates in the queue
-
-    while (!queue.isEmpty()) {
-        int x = queue.poll();
-        int i = x / n, j = x % n;
-        for (int k = 0; k < 4; ++k) {
-            int p = i + d[k], q = j + d[k + 1]; // empty room
-            if (0 <= p && p < m && 0 <= q && q < n && rooms[p][q] == Integer.MAX_VALUE) {
-                rooms[p][q] = rooms[i][j] + 1;
-                queue.offer(p * n + q);
-            }
+    for(int i = 0; i < rooms.length; i++) {
+        for(int j = 0; j < rooms[0].length; j++) {
+            if(rooms[i][j] == 0) {
+                dfs(rooms, i, j);
+            }              
         }
     }
 }
+
+public void dfs(int[][] rooms, int i, int j) {
+    for(int[] dir : dirs) {
+        int x = i + dir[0], y = j + dir[1];
+        if(x < 0 || x >= rooms.length || y < 0 || y >= rooms[0].length || rooms[x][y] <= rooms[i][j]) continue;
+        rooms[x][y] = rooms[i][j] + 1;
+        dfs(rooms, x, y);
+    }
+}
+
