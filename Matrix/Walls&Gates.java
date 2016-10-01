6cf53ae107c68d27c74ee61c6e3364f501337b74
Jiaxing <tian.jiaixng@gmail.com>
2016-10-01 13:52:32 -0400

    Update Walls&Gates.java

--- Matrix/Walls&Gates.java
+++ Matrix/Walls&Gates.java
@@ -49,3 +49,40 @@ public class Solution {
         }
     }
 }
+
+// Optimzed to travese for one time and offer all the gates to the queue
+// The points visited first is has small distance
+// The points with distance less than INF is visited before and has smaller distance
+public class Solution {
+    public void wallsAndGates(int[][] rooms) {
+        //Time Complexity O(NM) space O(MN)
+        if(rooms == null || rooms.length == 0 || rooms[0].length == 0){
+            return;
+        }
+        int n = rooms.length;
+        int m = rooms[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        
+        //Traverse the matrix first push all the gate to the queue
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(rooms[i][j] == 0){
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+        
+        int[][] dir = new int[][]{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        while(!queue.isEmpty()){
+            int[] step = queue.poll();
+            for(int k=0; k<4; k++){
+                int x = step[0] + dir[k][0];
+                int y = step[1] + dir[k][1];
+                if(x>=0 && x<n && y>=0 && y<m && rooms[x][y] == Integer.MAX_VALUE){
+                    rooms[x][y] = rooms[step[0]][step[1]] + 1;
+                    queue.offer(new int[]{x, y});
+                }
+            }
+        }
+    }
+}
