f0df43aec78910f610bbb1d61a33c239d36ae253
Jiaxing <tian.jiaixng@gmail.com>
2016-10-06 12:20:32 -0400

    Update LevelOrderTraversal.java

--- Tree/LevelOrderTraversal.java
+++ Tree/LevelOrderTraversal.java
@@ -36,3 +36,38 @@ public class Solution {
         return res;
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        if(root == null){
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            List<Integer> list = new LinkedList<>();
+            int size = queue.size();
+            for(int i=0; i<size; i++){
+                TreeNode cur = queue.poll();
+                list.add(cur.val);
+                if(cur.left != null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null){
+                    queue.offer(cur.right);
+                }
+            }
+            res.add(0, list);
+        }
+        return res;
+    }
+}
