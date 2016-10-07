982fbd01404840c9b50a183af3908a2c24ab1d78
Jiaxing <tian.jiaixng@gmail.com>
2016-10-07 15:07:20 -0400

    Update BST_Iterator.java

--- Tree/BST_Iterator.java
+++ Tree/BST_Iterator.java
@@ -7,7 +7,9 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-
+//Impliment with LinkedList to traverse the tree before head
+//O(N) -dfs; O(1) for hasNext() and next()
+//O(N) for space complexity
 public class BSTIterator {
     
     List<Integer> list = new LinkedList<>();
@@ -32,5 +34,30 @@ public class BSTIterator {
     /** @return the next smallest number */
     public int next() {
         return list.remove(0);
+    }
+}
+//Implement with stack
+//Space O(h) average Time O(1) worst O(h)
+public class BSTIterator {
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode tmpNode = stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+    
+    private void pushAll(TreeNode node) {
+        for (; node != null; stack.push(node), node = node.left);
     }
 }
