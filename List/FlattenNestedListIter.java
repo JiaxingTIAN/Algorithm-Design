12cf9807648540f195c0a53e42c216335f507206
Jiaxing <tian.jiaixng@gmail.com>
2016-10-08 03:52:24 -0400

    Update FlattenNestedListIter.java

--- List/FlattenNestedListIter.java
+++ List/FlattenNestedListIter.java
@@ -15,3 +15,41 @@
  *     public List<NestedInteger> getList();
  * }
  */
+public class NestedIterator implements Iterator<Integer> {
+    Stack<NestedInteger> stack;
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = new Stack<>();
+        for(int i=nestedList.size()-1; i>=0; i--){
+            stack.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if(hasNext()){
+            return stack.pop().getInteger();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while(!stack.isEmpty()){
+            NestedInteger cur = stack.peek();
+            if(cur.isInteger()){
+                return true;
+            }
+            stack.pop();
+            for(int i=cur.getList().size()-1; i>=0; i--){
+                stack.push(cur.getList().get(i));
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
