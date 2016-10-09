07ac6dcbf6e114da87fd3c85ecf0ef83a8365988
Jiaxing <tian.jiaixng@gmail.com>
2016-10-09 04:18:56 -0400

    Update PartitionEqualSubsetSum.java

--- DP/PartitionEqualSubsetSum.java
+++ DP/PartitionEqualSubsetSum.java
@@ -1,20 +1,28 @@
 public class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += num;
+        if(nums == null || nums.length == 0){
+            return true;
         }
-        if (sum % 2 == 0) {
-            sum /= 2;
-            boolean[] can = new boolean[sum + 1];
-            can[0] = true;
-            for (int num : nums) {
-                for (int i = sum; i >= num; i--) {
-                    can[i] |= can[i - num];
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+        }
+        if((sum & 1) == 1){
+            return false;
+        }
+        //Similar to backpack
+        boolean[][] canPartition = new boolean[sum/2 + 1][nums.length+1];
+        for(int i=0; i<=nums.length; i++){
+            canPartition[0][i] = true;
+        }
+        for(int i=1; i<=sum/2; i++){
+            for(int j=1; j<=nums.length; j++){
+                canPartition[i][j] = canPartition[i][j-1];
+                if(i >= nums[j-1]){
+                    canPartition[i][j] = canPartition[i - nums[j-1]][j-1] || canPartition[i][j];
                 }
             }
-            return can[sum];
         }
-        return false;
+        return canPartition[sum/2][nums.length];
     }
 }
