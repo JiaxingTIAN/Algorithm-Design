a394f58b27b75d6fd0a879b060551da2dc78e02f
Jiaxing <tian.jiaixng@gmail.com>
2016-10-09 04:48:35 -0400

    Update PartitionEqualSubsetSum.java

--- DP/PartitionEqualSubsetSum.java
+++ DP/PartitionEqualSubsetSum.java
@@ -1,28 +1,31 @@
 public class Solution {
     public boolean canPartition(int[] nums) {
         if(nums == null || nums.length == 0){
-            return true;
+            return false;
         }
         int sum = 0;
         for(int i=0; i<nums.length; i++){
             sum += nums[i];
         }
-        if((sum & 1) == 1){
-            return false;
+        if((sum&1)==1){
+            return false;   // If the total sum is odd, return false
         }
-        //Similar to backpack
-        boolean[][] canPartition = new boolean[sum/2 + 1][nums.length+1];
+        //Find the elements in the array such that total sum is sum/2
+        sum /= 2;
+        //Turn into sackpack problem
+        boolean [][]dp = new boolean[nums.length+1][sum+1]; //dp[i][j] first ith elem sum up to j
+        //Initialize all 0 value is true, 0 item is false
         for(int i=0; i<=nums.length; i++){
-            canPartition[0][i] = true;
+            dp[i][0] = true;
         }
-        for(int i=1; i<=sum/2; i++){
-            for(int j=1; j<=nums.length; j++){
-                canPartition[i][j] = canPartition[i][j-1];
-                if(i >= nums[j-1]){
-                    canPartition[i][j] = canPartition[i - nums[j-1]][j-1] || canPartition[i][j];
+        for(int i=1; i<=nums.length; i++){
+            for(int j=1; j<=sum; j++){
+                dp[i][j] = dp[i-1][j];  //Dont add ith item
+                if(j >= nums[i-1]){
+                    dp[i][j] |= dp[i-1][j - nums[i-1]];    //Add ith item
                 }
             }
         }
-        return canPartition[sum/2][nums.length];
+        return dp[nums.length][sum];
     }
 }
