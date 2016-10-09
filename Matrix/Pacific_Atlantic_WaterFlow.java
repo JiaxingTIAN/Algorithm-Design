97e48ff1d4ae9812a0857477abc91c361272025f
Jiaxing <tian.jiaixng@gmail.com>
2016-10-09 13:23:45 -0400

    Update Pacific_Atlantic_WaterFlow.java

--- Matrix/Pacific_Atlantic_WaterFlow.java
+++ Matrix/Pacific_Atlantic_WaterFlow.java
@@ -1,51 +1,38 @@
+//DFS
 public class Solution {
-    private void dfs(int[][] matrix, boolean[][] map, int height, int row, int col) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        if (row < 0 || row >= m || col < 0 || col >= n) {
-            return;
-        }
-        if (map[row][col] || height > matrix[row][col]) {
-            return;
-        }
-        map[row][col] = true;
-        int[] shift = {0, 1, 0, -1, 0};
-        for (int i = 0; i < 4; i++) {
-            dfs(matrix, map, matrix[row][col], row + shift[i], col + shift[i + 1]);
-        }
-    }
     public List<int[]> pacificAtlantic(int[][] matrix) {
-        List<int[]> res = new LinkedList<int[]>();
-        int m = matrix.length;
-        if (m == 0) {
+        List<int[]> res = new LinkedList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
             return res;
         }
-        int n = matrix[0].length;
-        if (n == 0) {
-            return res;
-        }
-        boolean[][] pacific = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
+        int n = matrix.length, m = matrix[0].length;
+        boolean[][]pacific = new boolean[n][m];
+        boolean[][]atlantic = new boolean[n][m];
+        for(int i=0; i<n; i++){
             dfs(matrix, pacific, Integer.MIN_VALUE, i, 0);
+            dfs(matrix, atlantic, Integer.MIN_VALUE, i, m-1);
         }
-        for (int j = 0; j < n; j++) {
-            dfs(matrix, pacific, Integer.MIN_VALUE, 0, j);
+        for(int i=0; i<m; i++){
+            dfs(matrix, pacific, Integer.MIN_VALUE, 0, i);
+            dfs(matrix, atlantic, Integer.MIN_VALUE, n-1, i);
         }
-        boolean[][] atlantic = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            dfs(matrix, atlantic, Integer.MIN_VALUE, i, n - 1);
-        }
-        for (int j = 0; j < n; j++) {
-            dfs(matrix, atlantic, Integer.MIN_VALUE, m - 1, j);
-        }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < m; j++) 
+                if (pacific[i][j] && atlantic[i][j]) 
                     res.add(new int[] {i, j});
-                }
-            }
-        }
         return res;
+    }
+    
+    int[][]dir = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+    
+    public void dfs(int[][]matrix, boolean[][]visited, int height, int x, int y){
+        int n = matrix.length, m = matrix[0].length;
+        if(x<0 || x>=n || y<0 || y>=m || visited[x][y] || matrix[x][y] < height)
+            return;
+        visited[x][y] = true;
+        for(int[]d:dir){
+            dfs(matrix, visited, matrix[x][y], x+d[0], y+d[1]);
+        }
     }
 }
 //BFS
