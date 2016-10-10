a34123859ad84ac5169e495c88d987e5e4337557
Jiaxing <tian.jiaixng@gmail.com>
2016-10-10 02:21:47 -0400

    Update NumberOfUnconnectedComponent.java

--- Graph/NumberOfUnconnectedComponent.java
+++ Graph/NumberOfUnconnectedComponent.java
@@ -34,3 +34,39 @@ public class Solution {
         return count;
     }
 }
+
+public class Solution {
+    public int countComponents(int n, int[][] edges) {
+        if(edges == null || edges.length == 0 || edges[0].length == 0){
+            return n;
+        }
+        int count = 0;
+        Map<Integer, Set<Integer>> map = new HashMap<>();   //Adjecent list
+        boolean[] visit = new boolean[n];
+        for(int i=0; i<n; i++){
+            map.put(i, new HashSet<>());
+        }
+        for(int[] edge:edges){
+            //Add edge to both node, since undirected
+            map.get(edge[0]).add(edge[1]);
+            map.get(edge[1]).add(edge[0]);
+        }
+        
+        for(int i=0; i<n; i++){
+            if(!visit[i]){
+                count++;
+                dfs(map, visit, i);
+            }
+        }
+        return count;
+    }
+    public void dfs(Map<Integer,Set<Integer>> map, boolean[]visit, int cur){
+        if(visit[cur])
+            return;
+        visit[cur] = true;
+        for(int nei:map.get(cur)){
+            if(visit[nei]) continue;
+            dfs(map, visit, nei);
+        }
+    }
+}
