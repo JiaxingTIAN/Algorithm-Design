f04f8d12ee6b8aa9e869b2f3d1a355ce66058421
Jiaxing <tian.jiaixng@gmail.com>
2016-10-10 14:58:24 -0400

    Update KthLargest.java

--- KthLargest.java
+++ KthLargest.java
@@ -33,3 +33,49 @@ public class Solution {
         nums[j] = tmp;
     }
 }
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        mergeSort(nums, 0, nums.length - 1);
+        return nums[nums.length - k];
+    }
+    
+    public void mergeSort(int[]nums, int lo, int hi){
+        if(lo >= hi){
+            return;
+        }
+        int mid = lo + (hi - lo)/2;
+        //Divide further
+        mergeSort(nums, lo, mid);
+        mergeSort(nums, mid+1, hi);
+        merge(nums, lo, mid, hi);   //merge together
+    }
+    
+    public void merge(int[]nums, int lo, int mid, int hi){
+        int l = lo, r = mid+1, idx = lo;
+        int[] tmp = new int[nums.length];
+        while(l <= mid && r <= hi){
+            if(nums[l] < nums[r]){
+                tmp[idx++] = nums[l++];
+            }else{
+                tmp[idx++] = nums[r++];
+            }
+        }
+        while(l <= mid){
+            tmp[idx++] = nums[l++];
+        }
+        while(r <= hi){
+            tmp[idx++] = nums[r++];
+        }
+        for(int i=lo; i<=hi; i++)
+            nums[i] = tmp[i];
+    }
+    public void swap(int[]nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
