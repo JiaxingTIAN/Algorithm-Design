9bbe3c3fe608c2f2369ab53d39a4c40a2b35922a
Jiaxing <tian.jiaixng@gmail.com>
2016-10-11 19:03:10 -0400

    Update Serialize&DeserializeBT.java

--- Tree/Serialize&DeserializeBT.java
+++ Tree/Serialize&DeserializeBT.java
@@ -1,32 +1,53 @@
-public class Solution{
-  public String serialize(TreeNode node){
-    StringBuilder sb = new StringBuilder();
-    preOrder(node, sb);
-    return sb.toString();
-  }
-  public void preOrder(TreeNode node, StringBuilder sb){
-    if(node == null){
-      sb.append("#,");
-      return;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        preOrder(root, sb);
+        System.out.println(sb.toString());
+        return sb.toString();
     }
-    sb.append(node.val).append(",");
-    preOrder(node.left, sb);
-    preOrder(node.right, sb);
-  }
-  public TreeNode deserialize(String data){
-    String[] str = data.split(",");
-    Queue<String> queue = new LinkedList<>();
-    queue.addAll(Arrays.asList(str));
-    return buildTree(queue);
-  }
-  public TreeNode buildTree(Queue<String> queue){
-    String str = queue.poll();
-    if(str.equals("#")){
-      return null;
+    //Preorder traversal, mark null with #, seperate with ,
+    public void preOrder(TreeNode node, StringBuilder sb){
+        if(node == null){
+            sb.append("#,");
+            return;
+        }
+        sb.append(node.val + ",");
+        preOrder(node.left, sb);
+        preOrder(node.right, sb);
     }
-    TreeNode node = new TreeNode(Integer.valueOf(str));
-    node.left = buildTree(queue);
-    node.right = buildTree(queue);
-    return node;
-  }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] str = data.split(",");
+        //Store the strings in a queue
+        Queue<String> queue = new LinkedList<>();
+        queue.addAll(Arrays.asList(str));
+        return buildTree(queue);
+    }
+    
+    public TreeNode buildTree(Queue<String> queue){
+        String cur = queue.poll();
+        if(cur.equals("#")){
+            return null;
+        }
+        TreeNode node = new TreeNode(Integer.valueOf(cur));
+        node.left = buildTree(queue);
+        node.right = buildTree(queue);
+        return node;
+    }
 }
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
