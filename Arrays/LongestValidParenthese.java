592bbdd0407f04e0b62c4920d45e06dfe68b38de
Jiaxing <tian.jiaixng@gmail.com>
2016-10-15 20:35:48 -0400

    Update LongestValidParenthese.java

--- Arrays/LongestValidParenthese.java
+++ Arrays/LongestValidParenthese.java
@@ -31,3 +31,55 @@ public class Solution {
         return len;
     }
 }
+
+//Dynamic Programming Version
+public class Solution {
+    public int longestValidParentheses(String s) {
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        int max = 0;
+        int[]dp = new int[s.length()];
+        char[] ch = s.toCharArray();
+        for(int i=1; i<s.length(); i++){
+            if(ch[i] == ')'){
+                if(ch[i-1] == '('){
+                    dp[i] = (i >= 2)? dp[i-2] + 2: 2;
+                }else if(i-dp[i-1]-1 >= 0 && ch[i-dp[i-1]-1] == '('){
+                    dp[i] = dp[i-1] + 2;
+                    if(i - dp[i-1] - 2 >= 0){
+                        dp[i] += dp[i-dp[i-1]-2];
+                    }
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+        /*
+        Stack<Integer> stack = new Stack<>();   //Keep the invalid parenthese
+        for(int i=0; i<s.length(); i++){
+            char c = ch[i];
+            //First iterate to locate invalid parenthese location
+            if(c == '('){
+                stack.push(i);  //push to stack when (
+            }else{
+                if(!stack.isEmpty() && ch[stack.peek()] == '('){
+                    stack.pop();
+                }else{
+                    stack.push(i);
+                }
+            }
+        }
+        if(stack.isEmpty())
+            return s.length();
+        int len = 0, a = s.length(), b = 0; //start from n - stack.peek()
+        while(!stack.isEmpty()){
+            b = stack.pop();
+            len = Math.max(len, a - b - 1);
+            a = b;
+        }
+        len = Math.max(len, a); //for 0 - a
+        return len;
+        */
+    }
+}
