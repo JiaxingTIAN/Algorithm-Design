b8d9f1ca36b09f09b4916ce7366fbbdcd5eaf8d8
Jiaxing <tian.jiaixng@gmail.com>
2016-10-18 03:51:27 -0400

    Update LongestIncSubsequenceII.java

--- Matrix/LongestIncSubsequenceII.java
+++ Matrix/LongestIncSubsequenceII.java
@@ -1,43 +1,33 @@
 public class Solution {
-    /**
-     * @param A an integer matrix
-     * @return  an integer
-     */
-    int[][] step = new int[][]{{1,0},{-1,0},{0,1},{0,-1}}; 
-    // Memorize O(n*m) time complexity
-    public int longestIncreasingContinuousSubsequenceII(int[][] A) {
-        // Write your code here
-        if(A == null || A.length == 0)
+    public int longestIncreasingPath(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
             return 0;
-        int[][]dp = new int[A.length][A[0].length];
-        
+        int n = matrix.length, m = matrix[0].length;
+        int [][] cache = new int[n][m]; //cache the result longest path for each cell 
         int max = 0;
-        for(int i=0;i<A.length;i++){
-            for(int j=0;j<A[0].length;j++){
-                dp[i][j] = search(A, i, j);  
-                max = Math.max(max, dp[i][j]);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){ //DFS start from each cell
+                int len = dfs(matrix, cache, i, j);
+                max = Math.max(len, max);
             }
         }
-        
         return max;
     }
-    
-    public int search(int[][] nums, int x, int y){
-        if(dp[x][y]!=0)
-            return dp[x][y];
-            
-        int ans = 1;
-        for(int[] d:step){
-            int dx = x + d[0];
-            int dy = y + d[1];
-            
-            if(dx<0||dx>=nums.length||dy<0||dy>=nums[0].length)
-                continue;
-            if(nums[x][y] > nums[dx][dy]){
-                ans = Math.max(ans,search(nums, dx, dy)+1);
-            }
+    public final int[][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+    public int dfs(int[][]matrix, int[][] cache, int x, int y){
+        if(cache[x][y] != 0){   //visited before
+            return cache[x][y];
         }
-        dp[x][y] = ans;
-        return ans;
+        int max = 1, n = matrix.length, m = matrix[0].length;
+        for(int[]d:dir){
+            int i = x + d[0];
+            int j = y + d[1];
+            if(i < 0 || i >= n || j < 0 || j >= m || matrix[i][j] <= matrix[x][y])
+                continue;
+            int len = 1 + dfs(matrix, cache, i, j);
+            max = Math.max(max, len);   //Max length from the four direction.
+        }
+        cache[x][y] = max;
+        return max;
     }
 }
