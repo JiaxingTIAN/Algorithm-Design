1e9f3473c707f5a3b8af248f7a91f8bd8782449c
Jiaxing <tian.jiaixng@gmail.com>
2016-10-20 23:57:16 -0400

    Update Combination Sum IV.java

--- DP/Combination Sum IV.java
+++ DP/Combination Sum IV.java
@@ -1,4 +1,5 @@
 public class Solution {
+    
     public int combinationSum4(int[] nums, int target) {
         if(target == 0){
             return 1;   //base case target value 0 <= [0] 
@@ -12,3 +13,29 @@ public class Solution {
         return res;
     }
 }
+
+//A better version with memoization
+public class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int[]dp = new int[target+1]; //Memoization Top Down
+        Arrays.fill(dp, -1);    //fill with -1 as unvisited
+        dp[0] = 1;  //Padding with 0, base case for target = 0 only [0]
+        return dfs(nums, dp, target);
+    }
+    public int dfs(int[]nums, int[]dp, int target){
+        if(target < 0) return 0;
+        if(dp[target] != -1)    //Visited Before
+            return dp[target];
+        dp[target] = 0;
+        for(int i=0; i<nums.length; i++){   
+            //Pick each element from the array
+            dp[target] += dfs(nums, dp, target - nums[i]);
+        }
+        return dp[target];
+    }
+}
+
+//Bottom Up Solution with DP
