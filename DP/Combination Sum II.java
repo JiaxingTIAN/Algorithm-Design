35078168204cd7e2eadda9591b8f9b4009ca1c07
Jiaxing <tian.jiaixng@gmail.com>
2016-10-21 01:40:59 -0400

    Update Combination Sum II.java

--- DP/Combination Sum II.java
+++ DP/Combination Sum II.java
@@ -9,7 +9,7 @@ public class Solution {
         dfs(res, new ArrayList<>(), candidates, target, 0, 0);
         return res;
     }
-    //DFS with backtrack O(target^n) 
+    //DFS with backtrack O(2^n) for each item either use once or not used
     public void dfs(List<List<Integer>> res, List<Integer> list, int[] cands, int target, int sum, int idx){
         if(sum == target){  //Base case add to list when equal
             res.add(new ArrayList(list));
