6fcac825ba1829072a57b4c9d7f72c0e85562a49
Jiaxing <tian.jiaixng@gmail.com>
2016-10-21 16:33:39 -0400

    Update power.java

--- power.java
+++ power.java
@@ -15,17 +15,20 @@ public class Solution {
 }
 
 public class Solution {
-    public double myPow(double x, int n) {
-        if(n==0) return 1;
-        if(n<0) {
-            n = -n;
+    public double myPow(double x, int n){
+        if(n == 0){ //Base case n = 0 return 1
+            return 1;
+        }
+        if(n < 0){  //if n is negative invert x
             x = 1/x;
+            n = -n;
         }
         double ans = 1;
-        while(n>0){
-            if((n&1)==1) ans *= x;
-            x *= x;
-            n >>= 1;
+        while(n > 0){
+            if(n%2 == 1)    //if n is odd
+                ans *= x;   //mul the extra x
+            x *= x;         //double x
+            n >>= 1;        //n = n/2
         }
         return ans;
     }
