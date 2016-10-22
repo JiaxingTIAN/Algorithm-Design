0027368aac77f76070364499bc6c52a9b70539eb
Jiaxing <tian.jiaixng@gmail.com>
2016-10-21 21:37:07 -0400

    Update modular.java

--- modular.java
+++ modular.java
@@ -8,3 +8,29 @@ public static int module(int x, int n, int m){
             return (x%m * module(x, n-1, m))%m;
         }
     }
+
+
+public class Solution {
+    private static final int M = 1337;
+
+    public int normalPow(int a, int b) {
+        int result = 1;
+        while (b != 0) {
+            if (b % 2 != 0)
+                result = result * a % M;
+            a = a * a % M;
+            b /= 2;
+        }
+        return result;
+    }
+
+    public int superPow(int a, int[] b) {
+        a %= M;
+        int result = 1;
+        for (int i = b.length - 1; i >= 0; i--) {
+            result = result * normalPow(a, b[i]) % M;
+            a = normalPow(a, 10);
+        }
+        return result;
+    }
+}
