8785a46559aea376a4aa73685ce1cdf359d2f6a4
Jiaxing <tian.jiaixng@gmail.com>
2016-10-23 14:39:57 -0400

    Update RangeSumQueryMutable.java

--- Tree/RangeSumQueryMutable.java
+++ Tree/RangeSumQueryMutable.java
@@ -1,4 +1,36 @@
 public class NumArray {
+    int[]num;
+    int[]sum;   //prefix sum good for less update operation
+    public NumArray(int[] nums) {   //O(N) build
+        num = nums;
+        sum = new int[nums.length+1];
+        for(int i=1; i<=nums.length; i++)
+            sum[i] = sum[i-1] + num[i-1];
+    }
+
+    void update(int i, int val) {
+        //O(N)
+        for(int j=i+1; j<sum.length; j++){
+            sum[j] = sum[j] + val - num[i];
+        }
+        num[i] = val;
+    }
+
+    public int sumRange(int i, int j) {
+        //Constant retrieve
+        return sum[j+1] - sum[i];
+    }
+}
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.update(1, 10);
+// numArray.sumRange(1, 2);
+
+
+public class NumArray {
     //Binary Index Tree
     int[] nums;
     int[] BIT;
