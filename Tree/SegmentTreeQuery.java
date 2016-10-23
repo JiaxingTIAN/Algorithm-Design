04185ce7c5bd6918b79878fb40181a02bb70cfc5
Jiaxing <tian.jiaixng@gmail.com>
2016-10-23 10:56:33 -0400

    Update SegmentTreeQuery.java

--- Tree/SegmentTreeQuery.java
+++ Tree/SegmentTreeQuery.java
@@ -1,3 +1,16 @@
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end, max;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end, int max) {
+ *         this.start = start;
+ *         this.end = end;
+ *         this.max = max
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
 public class Solution {
     /**
      *@param root, start, end: The root of segment tree and 
@@ -6,19 +19,54 @@ public class Solution {
      */
     public int query(SegmentTreeNode root, int start, int end) {
         // write your code here
-        if(start == root.start && root.end == end)  // 相等 
+        if(start > end || root == null)
+            return Integer.MIN_VALUE;
+        if(root.start >= start && root.end <= end){
             return root.max;
-        
-        int mid = (root.start + root.end)/2;
-        int leftmax = Integer.MIN_VALUE, rightmax = Integer.MIN_VALUE;
-        // 左子区
-        if(start <= mid) 
-            leftmax = query(root.left, start, Math.min(mid,end));
-        
-        // 右子区
-        if(mid < end)
-            rightmax = query(root.right, Math.max(mid+1,start), end);
-        // else 就是不相交
-        return Math.max(leftmax, rightmax);
+        }
+        int left = Integer.MIN_VALUE, right = Integer.MIN_VALUE;
+        int mid = root.start + (root.end - root.start)/2;
+        if(start <= mid)
+            left = query(root.left, start, Math.min(end, mid));
+        if(end > mid)
+            right = query(root.right, Math.max(mid+1, start), end);
+        return Math.max(left, right);
+    }
+}
+
+/**
+ * Definition of SegmentTreeNode:
+ * public class SegmentTreeNode {
+ *     public int start, end, count;
+ *     public SegmentTreeNode left, right;
+ *     public SegmentTreeNode(int start, int end, int count) {
+ *         this.start = start;
+ *         this.end = end;
+ *         this.count = count;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     *@param root, start, end: The root of segment tree and 
+     *                         an segment / interval
+     *@return: The count number in the interval [start, end]
+     */
+    public int query(SegmentTreeNode root, int start, int end) {
+        // write your code here
+        if(start > end || root == null)    return 0;
+        if(start <= root.start && end >= root.end){
+            return root.count;
+        }
+        int left = 0, right = 0;
+        int mid = root.start + (root.end - root.start)/2;
+        if(start <= mid){
+            left = query(root.left, start, Math.min(end, mid));
+        }
+        if(end > mid){
+            right = query(root.right, Math.max(mid+1, start), end);
+        }
+        return left + right;
     }
 }
