07b31139c925aab75f36758da5039063f36c390f
Jiaxing <tian.jiaixng@gmail.com>
2016-10-23 14:27:28 -0400

    Update RangeSum.java

--- Tree/RangeSum.java
+++ Tree/RangeSum.java
@@ -1,40 +1,21 @@
-	int[] nums;
-	int[] BIT;
-	int n;
+public class NumArray {
+    private int[] sum;  //In place optimization
+    public NumArray(int[] nums) {
+        //sum = new int[nums.length + 1];
+        for(int i=1; i<nums.length; i++){
+            nums[i] = nums[i-1] + nums[i]; 
+        }
+        sum = nums;
+    }
 
-	public NumArray(int[] nums) {
-		this.nums = nums;
+    public int sumRange(int i, int j) {
+        if(i == 0) return sum[j];
+        return sum[j] - sum[i-1];
+    }
+}
 
-		n = nums.length;
-		BIT = new int[n + 1];
-		for (int i = 0; i < n; i++)
-			init(i, nums[i]);
-	}
 
-	public void init(int i, int val) {
-		i++;
-		while (i <= n) {
-			BIT[i] += val;
-			i += (i & -i);
-		}
-	}
-
-	void update(int i, int val) {
-		int diff = val - nums[i];
-		nums[i] = val;
-		init(i, diff);
-	}
-
-	public int getSum(int i) {
-		int sum = 0;
-		i++;
-		while (i > 0) {
-			sum += BIT[i];
-			i -= (i & -i);
-		}
-		return sum;
-	}
-
-	public int sumRange(int i, int j) {
-		return getSum(j) - getSum(i - 1);
-	}
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray = new NumArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
