1373242c5a8c6c4dca33f468c9eac125c4500591
Jiaxing <tian.jiaixng@gmail.com>
2016-10-25 11:28:32 -0400

    Update PathSumIII.java

--- Tree/PathSumIII.java
+++ Tree/PathSumIII.java
@@ -8,6 +8,7 @@
  * }
  */
 public class Solution {
+    //O(N^2)
     public int pathSum(TreeNode root, int sum) {
         if(root == null)
             return 0;
@@ -22,6 +23,27 @@ public class Solution {
             res++;
         res += findPath(root.left, sum - root.val);
         res += findPath(root.right, sum - root.val);
+        return res;
+    }
+    
+    //Time Complexity O(N)
+    public int pathSum2(TreeNode root, int sum) {
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        return helper(root, 0, sum, preSum);
+    }
+    
+    public int helper(TreeNode root, int sum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) {
+            return 0;
+        }
+        
+        sum += root.val;
+        int res = preSum.getOrDefault(sum - target, 0);
+        preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        
+        res += helper(root.left, sum, target, preSum) + helper(root.right, sum, target, preSum);
+        preSum.put(sum, preSum.get(sum) - 1);
         return res;
     }
    
