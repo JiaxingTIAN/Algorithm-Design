4da7fa8f8795aa36e4e3b8591c8b860a92a488d9
Jiaxing <tian.jiaixng@gmail.com>
2016-10-27 14:22:27 -0400

    Update PathSum.java

--- Tree/PathSum.java
+++ Tree/PathSum.java
@@ -20,3 +20,14 @@ public class Solution {
         return pathSum(node.left, cur, sum) || pathSum(node.right, cur, sum);
     }
 }
+
+
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
+        if(root.left == null && root.right == null)
+            return sum == root.val;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
