2040dc645b7d1efa0ed4a4a6195ae65edc5a2b46
Jiaxing <tian.jiaixng@gmail.com>
2016-10-29 17:06:52 -0400

    Update ClosetBSTvalue.java

--- Tree/ClosetBSTvalue.java
+++ Tree/ClosetBSTvalue.java
@@ -29,3 +29,28 @@ public class Solution {
         dfs(node.right, target);
     }
 }
+//Time Complexity => O(logn)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int closestValue(TreeNode root, double target) {
+        int res = root.val;
+        double diff = Math.abs(target - res);
+        while(root != null){
+            double d = Math.abs(root.val - target);
+            if(d < diff){
+                res = root.val;
+                diff = d;
+            }
+            root = target > root.val? root.right:root.left;
+        }
+        return res;
+    }
+}
