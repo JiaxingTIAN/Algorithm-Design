d3631764d6fb162b59ec1276c06cf79749fca151
Jiaxing <tian.jiaixng@gmail.com>
2016-10-30 23:06:30 -0400

    Update UniqueBST.java

--- Tree/UniqueBST.java
+++ Tree/UniqueBST.java
@@ -63,3 +63,42 @@ public class Solution {
         System.out.println(ans.size());
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if(n==0) 
+            return new ArrayList<TreeNode>();
+        return generateNode(1,n);
+    }
+    public List<TreeNode> generateNode(int start, int end){
+        List<TreeNode> list = new ArrayList();
+        if(start > end){
+            list.add(null);
+            return list;
+        }
+        //Try for every value
+        for(int i=start;i<=end;i++){
+            List<TreeNode> left = generateNode(start, i-1);
+            List<TreeNode> right = generateNode(i+1, end);
+            for(TreeNode l:left){
+                for(TreeNode r:right){
+                    TreeNode root = new TreeNode(i);
+                    root.left = l;
+                    root.right = r;
+                    list.add(root);
+                }
+            }
+        }
+        return list;
+    }
+}
