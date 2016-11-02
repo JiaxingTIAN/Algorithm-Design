927fe00fff4cd13cb7c1ee19ac64abcd41342495
Jiaxing <tian.jiaixng@gmail.com>
2016-11-02 03:20:45 -0400

    Update PalindromePermutation.java

--- String/PalindromePermutation.java
+++ String/PalindromePermutation.java
@@ -18,3 +18,21 @@ public class Solution {
         return true;
     }
 }
+
+
+public class Solution {
+    public boolean canPermutePalindrome(String s) {
+        if(s == null || s.length() == 0){
+            return true;
+        }
+        Set<Character> set = new HashSet<>();
+        for(char c:s.toCharArray()){
+            if(set.contains(c)){
+                set.remove(c);
+            }else{
+                set.add(c);
+            }
+        }
+        return set.size() == 0 || set.size() == 1;
+    }
+}
