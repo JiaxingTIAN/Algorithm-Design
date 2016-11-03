f63225af92630066551e56bdcfa568a9991da835
Jiaxing <tian.jiaixng@gmail.com>
2016-11-03 18:05:29 -0400

    Update FlipGame.java

--- String/FlipGame.java
+++ String/FlipGame.java
@@ -13,3 +13,28 @@ public class Solution {
         return false;
     }
 }
+//Basic Recursion O(N!!) n * n-2 * n-4 * n-6
+//Use a HashMap to store string and reuslt for that string
+//<String, Boolean> Pair 
+//Total case 2^n hence O(2^n)
+public class Solution {
+    public boolean canWin(String s) {
+        Map<String, Boolean> map = new HashMap<>();
+        return win(map, s);
+    }
+    public boolean win(Map<String, Boolean> map, String s){
+        if(map.containsKey(s)) 
+            return map.get(s);
+        for(int i=0; i<s.length()-1; i++){
+            if(s.startsWith("++", i)){
+                String tmp = s.substring(0, i) + "--" + s.substring(i+2);
+                if(!win(map, tmp)){
+                    map.put(s, true);
+                    return true;
+                }
+            }
+        }
+        map.put(s, false);
+        return false;
+    }
+}
