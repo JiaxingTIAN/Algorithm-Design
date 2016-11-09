dcbd59f4c1150c5603054d2aceca8d47e653e6f0
Jiaxing <tian.jiaixng@gmail.com>
2016-11-09 01:12:33 -0500

    Update LongestKdistinct.java

--- String/LongestKdistinct.java
+++ String/LongestKdistinct.java
@@ -20,3 +20,27 @@ public class Solution {
         return max;
     }
 }
+
+
+public class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if(s == null || s.length() == 0){
+            return 0;
+        }
+        int max = 0;
+        int n = s.length();
+        char[] ch = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0, j=0; j<n; j++){
+            map.put(ch[j], map.getOrDefault(ch[j], 0)+1);
+            while(map.size() > k){
+                map.put(ch[i], map.get(ch[i])-1);
+                if(map.get(ch[i]) == 0)
+                    map.remove(ch[i]);
+                i++;
+            }
+            max = Math.max(max, j-i+1);
+        }
+        return max;
+    }
+}
