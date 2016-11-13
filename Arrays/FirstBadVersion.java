7f2635496975d661e550887da220219143260b0a
Jiaxing <tian.jiaixng@gmail.com>
2016-11-13 02:55:04 -0500

    Create FirstBadVersion.java

---
+++ Arrays/FirstBadVersion.java
@@ -0,0 +1,18 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+        int i = 1, j = n;
+        while(i < j){
+            int mid = i + (j - i)/2;
+            if(isBadVersion(mid)){
+                j = mid;
+            }else{
+                i = mid + 1;
+            }
+        }
+        return j;
+    }
+}
