663909c514bce33b347b06d7101a03f39ffbe0d2
Jiaxing <tian.jiaixng@gmail.com>
2016-11-14 07:15:32 -0500

    Update ConnectedComponent.java

--- Graph/ConnectedComponent.java
+++ Graph/ConnectedComponent.java
@@ -1,75 +1,76 @@
-public class Solution{
-  class UnionFind{
-    HashMap<Integer, Integer> father = new HashMap<>();
-    //Initialize the stucture
-    UnionFind(HashSet<Integer> hashset){
-      for(Integer i:hashset){
-        father.put(i, i);
-      }
-    }
-    //find the big boss---identifier of the component
-    int find(int x){
-      int parent = father.get(x);
-      while(parent != fahter.get(parent))
-        parent = father.get(parent);
-      return parent;
-    }
-    
-    int compressed_find(int x){
-      int parent = father.get(x);
-      while(parent != father.get(parent))
-        parent = father.get(parent);
-      
-      while(x != father.get(x)){
-        int next = father.get(x);
-        father.put(x, parent);
-        x = next;
-      }
-      return parent;
-    }
-    
-    void union(int x, int y){
-      int fa_x = find(x);
-      int fa_y = find(y);
-      if(fa_x != fa_y)
-        father.put(fa_x, fa_y);
-    }
-  }
-  
-  List<List<Integer>> print(HashSet<Integer> hashSet, UnionFind uf, int n){
-    List<List<Integer>> ans = new ArrayList<>();
-    Map<Integer, List<Integer>> hashMap = new HashMap<>();
-    for(int i:hashSet){
-      int fa = uf.find(i);
-      hashMap.putIfAbsent(fa, new ArrayList<Integer>());
-      hashMap.get(fa).add(i);
-    }
-    for(List<Integer> now：hashMap.values()){
-      Collections.sort(now);
-      ans.add(now);
-    }
-    return ans;
-  }
-  
-  public List<List<Integer>> connectedSet(ArrayList<DirectedGraphNode> nodes){
-    HashSet<Integer> hashSet = new HashSet<Integer>();
-    for(DirectedGraphNode now:nodes){
-      hashSet.add(now.label);
-      for(DirectedGraphNode neighbour:now.neighbors){
-        hashSet.add(neighbour.label);
-      }
-    }
-    UnionFind uf = new UnionFind(hashSet);
-    
-    for(DirectedGraphNode now:nodes){
-      for(DirectedGraphNode neighbor:now.neighbors){
-        int fnow = uf.find(now.label);
-        int fneighbor = uf.find(neighbor.label);
-        if(fnow!=fneighbor){
-          uf.union(now.label, neighbor.label);
+/**
+ * Definition for Undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     ArrayList<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    /**
+     * @param nodes a array of Undirected graph node
+     * @return a connected set of a Undirected graph
+     */
+     
+    public class UnionFind{
+        private Map<Integer, Integer> root;
+        
+        public UnionFind(Set<Integer> set){
+            root = new HashMap<>();
+            for(Integer num:set){
+                root.put(num, num);
+            }
         }
-      }
+        
+        public int find(int num){
+            int parent = root.get(num);
+            while(parent!=root.get(parent)){
+                parent = root.get(parent);
+            }
+            while(num!=root.get(num)){
+                int next = root.get(num);
+                root.put(num, parent);
+                num = next;
+            }
+            return parent;
+        }
+        
+        public void union(int x, int y){
+            int root_x = find(x);
+            int root_y = find(y);
+            if(root_x!=root_y)
+                root.put(root_x, root_y);
+        }
     }
-    return print(hashSet, uf, nodes.size());
-  }
+    public List<List<Integer>> connectedSet(ArrayList<UndirectedGraphNode> nodes) {
+        // Write your code here
+        Set<Integer> set = new HashSet<Integer>();
+        for(UndirectedGraphNode node:nodes){
+            set.add(node.label);
+            for(UndirectedGraphNode nei:node.neighbors)
+                set.add(nei.label);
+        }
+        
+        UnionFind uf = new UnionFind(set);
+        for(UndirectedGraphNode node:nodes)
+            for(UndirectedGraphNode nei:node.neighbors)
+                uf.union(node.label, nei.label);
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int num:set){
+            int root = uf.find(num);
+            if(!map.containsKey(root))
+                map.put(root, new ArrayList<Integer>());
+            map.get(root).add(num);
+        }
+        
+        for(List<Integer> list:map.values()){
+            Collections.sort(list);
+            ans.add(list);
+        }
+        
+        return ans;
+    }
+    
 }
