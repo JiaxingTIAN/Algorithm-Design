84bbbb57284191c3cb9dde8d93e0ce9f3eb421a5
Jiaxing <tian.jiaixng@gmail.com>
2016-11-17 23:54:17 -0800

    Update RangeSum.java

--- Matrix/RangeSum.java
+++ Matrix/RangeSum.java
@@ -1,19 +1,43 @@
 public class NumMatrix {
-
-    private int [][] sum;
+    int[][]bit;
+    int[][]nums;
+    int n, m;
     public NumMatrix(int[][] matrix) {
-        if(matrix.length!=0)sum = new int[matrix.length+1][matrix[0].length+1];
-        for(int i=0;i<matrix.length;i++){
-            int s = 0;
-            for(int j=0;j<matrix[0].length;j++){
-                s += matrix[i][j];
-                sum[i+1][j+1] = s+sum[i][j+1];
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+        n = matrix.length;
+        m = matrix[0].length;
+        bit = new int[n+1][m+1];
+        nums = new int[n][m];
+        for(int i=0; i<n; i++)
+            for(int j=0; j<m; j++)
+                update(i, j, matrix[i][j]);
+    }
+
+    public void update(int row, int col, int val) {
+        int diff = val - nums[row][col];
+        nums[row][col] = val;
+        for(int i=row+1; i<=n; i += (i&-i)){
+            for(int j=col+1; j<=m; j += (j&-j)){
+                bit[i][j] += diff;
             }
         }
-        
     }
-
+    
+    public int getSum(int r, int c){
+        int res = 0;
+        for(int i=r+1; i>0; i -= (i&-i))
+            for(int j=c+1; j>0; j -= (j&-j))
+                res += bit[i][j];
+        return res;
+    }
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum[row2+1][col2+1] - sum[row1][col2+1] - sum[row2+1][col1] + sum[row1][col1];
+        return getSum(row2, col2) - getSum(row2, col1-1) - getSum(row1-1, col2) + getSum(row1-1, col1-1);
     }
 }
+
+
+// Your NumMatrix object will be instantiated and called as such:
+// NumMatrix numMatrix = new NumMatrix(matrix);
+// numMatrix.sumRegion(0, 1, 2, 3);
+// numMatrix.update(1, 1, 10);
+// numMatrix.sumRegion(1, 2, 3, 4);
